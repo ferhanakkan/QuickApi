d00356904a85bf354b9583dcaed30c30ca902323
Ferhan Akkan <ferhan.akkan@portalgrup.com>
2020-10-03 18:05:19 +0300

    App delegate editted

--- Example/QuickApi/AppDelegate.swift
+++ Example/QuickApi/AppDelegate.swift
@@ -22,6 +22,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         Quick.shared.timeOutTime = 10 // 10 Second. You have to set timeOutTime. İt accept seconds as Int.
         Quick.shared.showResponseJSONOnConsole = true // If you want to see your response data on console you have to set it true. It's totaly optional. It's preset false.
         Quick.shared.showLoadingInducator = true // If you want to see loading inducator while request continues you have to set it true. It's totaly optional. It's preset false.
+        Quick.shared.acceptLanguageCode = "tr" // If you want you can also add Accept language to your Http Header. You can do this actions also in another part of app. It will be saved on local database.
+        Quick.shared.setToken(token: "123123") // If you want you can also add Accept language to your Http Header. You can do this actions also in another part of app. It will be saved on local database.
         LoadingView.loadingBackgroundColor = UIColor.darkGray.withAlphaComponent(0.8) // It's preset color of main background color of Loading View it also can be changed if you prefer.
         LoadingView.loadingSubViewBackgroundColor = UIColor.lightGray // It's preset color of inducator background color of Loading View it also can be changed if you prefer.
         
