d8e09aa72fbe40242d1f222743d50adb3b9a8f9e
Ferhan Akkan <>
2021-09-30 03:22:14 +0300

    Refactor

--- Package.swift
+++ Package.swift
@@ -6,14 +6,10 @@ import PackageDescription
 let package = Package(
   name: "QuickApi",
   platforms: [
-    // Add support for all platforms starting from a specific version.
     .macOS(.v10_15),
     .iOS(.v11),
-    //    .watchOS(.v5),
-    //    .tvOS(.v11)
   ],
   products: [
-    // Products define the executables and libraries a package produces, and make them visible to other packages.
     .library(
       name: "QuickApi",
       targets: ["QuickApi"]),
@@ -22,8 +18,6 @@ let package = Package(
     .package(url: "https://github.com/Alamofire/Alamofire.git", .upToNextMajor(from: "5.4.0"))
   ],
   targets: [
-    // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-    // Targets can depend on other targets in this package, and on products in packages this package depends on.
     .target(
       name: "QuickApi",
       dependencies: ["Alamofire"]),
